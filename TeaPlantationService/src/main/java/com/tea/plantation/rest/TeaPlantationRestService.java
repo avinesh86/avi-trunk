9bb3ca794fe2ec48d217045274f9ab7d400ce06e
shashikap <shashikabokks@gmail.com>
2016-03-21 00:04:19 +0800

    adding some features

--- TeaPlantationService/src/main/java/com/tea/plantation/rest/TeaPlantationRestService.java
+++ TeaPlantationService/src/main/java/com/tea/plantation/rest/TeaPlantationRestService.java
@@ -9,6 +9,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.tea.plantation.PermEmpSalary;
+import com.tea.plantation.Track;
 
 @Path("/teaPlantation/")
 public class TeaPlantationRestService {
@@ -32,8 +33,28 @@ public class TeaPlantationRestService {
 	public Response createTrackInJSON(PermEmpSalary permEmpSalary) {
 
 		String result = "Track saved : " + permEmpSalary;
+		System.out.println("result categoryID: " + permEmpSalary.getCategoryID());
 		return Response.status(201).entity(result).build();
 		
+	}
+
+	@POST
+	@Path("/test/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response test(Track track) {
+
+		String result = "Track saved : " + track;
+		System.out.println("result categoryID: " + track.getSinger());
+		return Response.status(201).entity(result).build();
+/*
+  {
+ "categoryID":"2",
+ "noOfEmp":"3",
+ "workingHrs":"8.0",
+ "unitAmount":"6"
+ }
+ */
+
 	}
 	
 }
