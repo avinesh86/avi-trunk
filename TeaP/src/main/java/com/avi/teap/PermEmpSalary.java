81762ea725bd1b9a4afd31d271fa2dcd88c74afe
Navin Munasinghe <Navin Munasinghe>
2016-03-20 13:30:22 +0800

    My first commit

---
+++ TeaP/src/main/java/com/avi/teap/PermEmpSalary.java
@@ -0,0 +1,34 @@
+package com.avi.teap;
+
+public class PermEmpSalary {
+	
+	private long categoryID;
+	private long noOfEmp;
+	private double workingHrs;
+	private long unitAmount;
+	
+	public long getCategoryID() {
+		return categoryID;
+	}
+	public void setCategoryID(long categoryID) {
+		this.categoryID = categoryID;
+	}
+	public long getNoOfEmp() {
+		return noOfEmp;
+	}
+	public void setNoOfEmp(long noOfEmp) {
+		this.noOfEmp = noOfEmp;
+	}
+	public double getWorkingHrs() {
+		return workingHrs;
+	}
+	public void setWorkingHrs(double workingHrs) {
+		this.workingHrs = workingHrs;
+	}
+	public long getUnitAmount() {
+		return unitAmount;
+	}
+	public void setUnitAmount(long unitAmount) {
+		this.unitAmount = unitAmount;
+	}
+}
